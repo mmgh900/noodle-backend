96c37833e951c981f28baf060a79fc935875478a
Majid Yaghouti <majid.yaghouti@psg.network>
2020-09-10 00:14:16 +0430

    افزودن API برای درج کاربر
    استفاده از middleware برای دسترسی به داده‌های ورودی

--- apps/user-manager/routes.js
+++ apps/user-manager/routes.js
@@ -1,10 +1,15 @@
 const userCtrl = require('./controllers/user');
+const dataParser = require('maya-data-parser');
 
 module.exports = {
   '/users': {
     GET: {
       function: userCtrl.getUsers,
       middlewares: []
+    },
+    POST: {
+      function: userCtrl.createUser,
+      middlewares: [dataParser]
     }
   }
 };
