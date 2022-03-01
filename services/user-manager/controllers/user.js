bf8b5dbb9fedf08fb474d82d9d4be53079f740c8
M. Mahdi Gheysari <gheysari.mm@gmail.com>
2022-03-01 21:32:48 +0330

    - Adding JWT middleware and sample create user controller

--- services/user-manager/controllers/user.js
+++ services/user-manager/controllers/user.js
@@ -1,27 +1,40 @@
 const User = require('../models/user');
 const c = require('../config');
+const jwt = require('jsonwebtoken');
 
 function getUsers(req, res) {
-  let users = User.getAll();
-  res.statusCode = c.statusCodes.SUCCESS;
-  res.setHeader('Content-Type', c.contentTypes.JSON);
-  res.end(JSON.stringify({users}));
+    let users = User.getAll();
+    res.statusCode = c.statusCodes.SUCCESS;
+    console.log(req.user)
+    res.setHeader('Content-Type', c.contentTypes.JSON);
+    res.end(JSON.stringify({users}));
 }
 
 function createUser(req, res) {
-  let user = new User({
-    username: req.data.username,
-    password: req.data.password,
-    name: req.data.name || '',
-    family: req.data.family || ''
-  });
-  user.save();
-  res.statusCode = c.statusCodes.SUCCESS;
-  res.setHeader('Content-Type', c.contentTypes.JSON);
-  res.end(JSON.stringify(req.data));
+    let user = new User({
+        username: req.data.username,
+        password: req.data.password,
+        name: req.data.name
+    });
+    user.save();
+    // Create token
+    const token = jwt.sign(
+        {username: req.data.username},
+        c.authConfig.TOKEN_KEY,
+        {
+            expiresIn: "2h",
+        }
+    );
+    const response = {
+        username: req.data.username,
+        token
+    }
+    res.statusCode = c.statusCodes.SUCCESS;
+    res.setHeader('Content-Type', c.contentTypes.JSON);
+    res.end(JSON.stringify(response));
 }
 
 module.exports = {
-  getUsers,
-  createUser
+    getUsers,
+    createUser
 };
