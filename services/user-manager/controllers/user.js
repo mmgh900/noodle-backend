209f115137b0bae89db5b7ffb5cd5799e4380c25
M. Mahdi Gheysari <gheysari.mm@gmail.com>
2022-03-11 11:37:00 +0330

    - Installing responser - Handling user controller errors

--- services/user-manager/controllers/user.js
+++ services/user-manager/controllers/user.js
@@ -1,6 +1,5 @@
 const User = require('../models/user');
 const c = require('../config');
-const jwt = require('jsonwebtoken');
 const {
     hashPassword,
     comparePassword
@@ -12,79 +11,117 @@ const {
 } = require('http-status-codes');
 const {UserTypes} = require("noodle-user-authorization");
 const {createToken} = require("../utils/token-management");
+const responseSender = require("gheysari-resposer");
 
 
 async function login(req, res) {
-    const user = await User.get(req.data.username)
-    const token = createToken(user.username, user.type)
-    if (await comparePassword(req.data.password, user.password)) {
-        res.statusCode = StatusCodes.OK
-        res.setHeader('Content-Type', c.contentTypes.JSON);
-        res.end(JSON.stringify({
-            username: user?.username,
-            token
-        }));
-    } else {
-        res.statusCode = StatusCodes.FORBIDDEN
-        res.setHeader('Content-Type', c.contentTypes.JSON);
-        res.end(JSON.stringify({
-            message: "Your username or password is not valid!"
-        }));
+    try {
+        const user = await User.get(req.data.username)
+        const token = createToken(user.username, user.type)
+        const isPasswordValid = await comparePassword(req.data.password, user.password)
+        if (isPasswordValid) {
+            const result = {
+                username: user?.username,
+                token
+            }
+            responseSender.sendSuccessfulResponse(res, result)
+        } else {
+            responseSender.sendForbiddenResponse(res, "Your username or password is not valid!")
+        }
+    } catch (error) {
+        responseSender.sendInternalErrorResponse(res, error)
     }
+}
 
+async function getUser(req, res) {
+    try {
+        const user = await User.get(req.params.username);
+        responseSender.sendSuccessfulResponse(res, user)
+    } catch (error) {
+        responseSender.sendInternalErrorResponse(res, error)
+    }
 }
 
 async function getUsers(req, res) {
-    const type = req.query?.type
-    let users = await User.getAll(type);
-    res.statusCode = StatusCodes.OK
-    res.setHeader('Content-Type', c.contentTypes.JSON);
-    res.end(JSON.stringify({users}));
+    try {
+        const type = req.query.type
+        const users = await User.getAll(type);
+        responseSender.sendSuccessfulResponse(res, users)
+    } catch (error) {
+        responseSender.sendInternalErrorResponse(res, error)
+    }
 }
 
 function _checkForValidType(req, res) {
     // Only admins can create admins or supporters
     if ((req.data.type === UserTypes.admin || req.data.type === UserTypes.supporter)
         && req.user.type !== UserTypes.admin) {
-        res.statusCode = StatusCodes.FORBIDDEN
-        res.setHeader('Content-Type', c.contentTypes.JSON);
-        return res.end(JSON.stringify({
-            message: "Only admins can create admins or supporters"
-        }));
+        responseSender.sendForbiddenResponse(res, "Only admins can create admins or supporters")
     }
 }
 
 async function createUser(req, res) {
-    _checkForValidType(req, res);
-    const hashedPassword = await hashPassword(req.data.password);
     try {
+        _checkForValidType(req, res);
+        const hashedPassword = await hashPassword(req.data.password);
         await User.add({
             ...req.data,
             password: hashedPassword,
         })
-    } catch (e) {
-        if (parseInt(e.code) === 23505) {
+
+
+        const token = createToken(req.data.username, req.data.type)
+        const result = {
+            username: req.data.username,
+            token
+        }
+
+        responseSender.sendSuccessfulResponse(res, result)
+    } catch (error) {
+        if (parseInt(error.code) === 23505) {
             res.statusCode = StatusCodes.CONFLICT
             res.setHeader('Content-Type', c.contentTypes.JSON);
             return res.end(JSON.stringify({
                 message: "A user with this username already exists"
             }));
+        } else {
+            responseSender.sendInternalErrorResponse(res, error)
         }
+
     }
 
-    const token = createToken(req.data.username, req.data.type)
-
-    res.statusCode = StatusCodes.OK
-    res.setHeader('Content-Type', c.contentTypes.JSON);
-    return res.end(JSON.stringify({
-        username: req.data.username,
-        token
-    }));
 
 }
+
+async function editUser(req, res) {
+    try {
+        let user = await User.get(req.params.username)
+        await User.update({
+            ...user,
+            ...req.data
+        })
+        user = await User.get(req.query.username)
+        responseSender.sendSuccessfulResponse(res, user)
+    } catch (error) {
+        responseSender.sendInternalErrorResponse(res, error)
+    }
+}
+
+async function deleteUser(req, res) {
+    try {
+        await User.delete(req.params.username);
+        responseSender.sendSuccessfulResponse(res)
+    } catch (error) {
+        responseSender.sendInternalErrorResponse(res, error)
+    }
+}
+
 
 module.exports = {
     getUsers,
     createUser,
-    login
+    login,
+    editUser,
+    deleteUser,
+    getUser
 };
